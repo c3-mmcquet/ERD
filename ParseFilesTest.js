163eae4743893c54010f517bdf999b4cb5d1d22e
c3-ScottLittle <39919571+c3-ScottLittle@users.noreply.github.com>
2018-07-17 16:59:34 -0700

    Add files via upload

--- ParseFilesTest.js
+++ ParseFilesTest.js
@@ -1,12 +1,73 @@
 function parseFiles(files) {
+	var entityList;
 	var category;
 	for(var i = 0; i < files.length; i++) {
 		category = getCategory(files[i]);
 		// TODO: process into a type
+		if (category != null){
+			if (category == 'type'){
+
+			}
+			if (category == 'js'){
+
+			}
+			if (category == 'simpleMetric'){
+				
+			}
+			if (category == 'compoundMetric'){
+				
+			}
+		}
+
 	}
 	// TODO: what to return?
 }
 
 function getCategory(file) {
-	// TODO
+	var name = file.name;
+	var category;
+	if (name.endsWith('c3typ')) {
+		var reader = new FileReader();
+		var contents = reader.readAsText(file);
+		if (contents.includes('entity ')){
+			category = 'entityType'
+		}
+		else if (contents.includes('Analytic<')){
+			category = 'analytic'
+		}
+		else if (contents.includes(' transforms ')){
+			category = 'transform'
+		}
+		else if (contents.includes('Canonical<')){
+			category = 'canonical'
+		}
+		else if (contents.includes('TSDataFlowEvent<')){
+			category = 'simpleDFE'
+		}
+		else if (contents.includes('CompoundDataFlowEvent<')){
+			category = 'compoundDFE'
+		}
+	}
+	else if (name.endsWith('js')){
+		category = 'js';
+	}
+	else if (name.endsWith('json')){
+		var path = file.webkitRelativePath;
+
+		if (path.includes('SimpleMetric')){
+			var reader = new FileReader();
+			var contents = reader.readAsText(file);
+			if (contents.includes('tsDecl')){
+				category = 'tsDecl'
+			}
+			else { category = 'standardSimpleMetric' }
+		}
+		if (path.includes('CompoundMetric')){
+			category = 'compoundMetric';
+		}
+	}
+	else { category == null }
+	return category;
 }
+
+
