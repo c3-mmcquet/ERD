18a800d1a873aa0403ed227c4fd4ebe8d3cafea2
c3-mmcquet <40179004+c3-mmcquet@users.noreply.github.com>
2018-07-18 09:49:28 -0700

    Add files via upload

---
+++ ProcessEntities.js
@@ -0,0 +1,115 @@
+/* PROCESS FUNCTIONS */
+
+function processEntityType(file) {
+	var entityName = file.name.substring(0, file.name.indexOf('.'));
+
+	var reader = new FileReader();
+	var contents = reader.readAsText(file);
+
+	// WHY NOT JUST HAVE MIXINS BE A STRING?
+	if (contents.includes('mixes')) {
+		var mixins = findMixins(contents);
+	}
+
+	if (contents.includes('extends')) {
+		var extends = findExtends(contents);
+	}
+
+	var fields = findFields(contents, false);
+
+	/* js files will be linked at the end */
+
+	var entityType = new EntityType(entityName, contents, mixins, extends, fields, null)
+	return 
+}
+
+
+/* HELPER FUNCTIONS */
+
+function findMixins(contents) {
+	var beginMixins = contents.indexOf('mixes');
+	beginMixins += 6; // Skip over word "mixes"
+
+	var endMixins;
+	if (contents.indexOf('type key') != -1) {
+		endMixins = contents.indexOf('type key') - 1;
+	}
+	else if (contents.indexOf('schema name') != -1) {
+		endMixins = contents.indexOf('schema name') - 1;
+	}
+	else if (indexOf('transforms') != -1) {
+		endMixins = contents.indexOf('transforms' - 1;
+	}
+	else {
+		endMixins = contents.indexOf('{') - 1;
+	}
+
+	// The hard way to do this is to parse through each mixin and put it
+	// in an array. Nahhhhh.
+	return contents.substring(beginMixins, endMixins);
+}
+
+function findExtends(contents) {
+	var beginExtends = contents.indexOf('extends');
+	beginExtends += 8; // Skip over word "extends"
+
+	var endExtends;
+	var i = beginExtends;
+	while (contents.charAt(i) != ' ') {
+		i++
+	}
+	endExtends = i;
+
+	return contents.substring(beginExtends, endExtends);
+}
+
+function findFields(contents, isTransform) {
+	var pattern = /\n[a-z|\s]*:[a-z|\s]*\n/i;
+
+	var fieldStrings = contents.match(pattern);
+
+	var fields = [];
+
+	var fieldName = "";
+	var fieldType = "";
+	var j;
+	var k;
+
+	for (var i = 0; i < fieldStrings.length; i++) {
+		j = fieldStrings[i].search(/[a-z]/i);
+		k = j;
+
+		// Search for field name
+		while (fieldStrings[i].charAt(k) != ' ' &&
+			fieldStrings[i].charAt(k) != ':') {
+			k++;
+		}
+		fieldName = fieldStrings[i].substring(j,k);
+
+		// Search for field type
+		j = fieldStrings[i].indexOf(':') + 1;
+		while (fieldStrings[i].charAt(j) == ' ') {
+			j++;
+		}
+		k = j;
+		while (fieldStrings[i].charAt(k) != ' ') {
+			k++;
+		}
+		fieldType = fieldStrings[i].substring(j,k);
+
+		// Add entry to fields array
+		fields.push({
+    		key: fieldName,
+    		value: fieldType
+		});
+	}
+
+	return fields;
+
+}
+
+
+
+
+
+
